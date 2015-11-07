2a1e9735aa902e1d7ab79fe76775f842916facc1
Gustavo Freitas de Amorim <gustavofreitasamorim@gmail.com>
2015-11-07 15:51:21 -0200

    Atualização da Matriz para um WebServer

--- Matriz/src/java/Matriz/Main.java
+++ Matriz/src/java/Matriz/Main.java
@@ -1,16 +1,5 @@
 package Matriz;
 
-import DAO.ClienteDAO;
-import DAO.Gerenciador.GerenciadorBD;
-import DAO.ProdutoDAO;
-import DAO.VendaDAO;
-import Modelo.Cliente;
-import Modelo.Produto;
-import Modelo.Venda.ItemVenda;
-import Modelo.Venda.VendaBuilder;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.application.Platform;
 import javafx.stage.Stage;
 
